5e45e03f34e7ead5b6dd60c8e4a6929e2b8cf0cb
Anuj <anuj22oj@gmail.com>
2026-04-05 16:15:12 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,30 @@
+const countryList = {
+  USD: "US", // United States Dollar
+  INR: "IN", // Indian Rupee
+  EUR: "FR", // Euro (Using France for flag)
+  GBP: "GB", // British Pound
+  JPY: "JP", // Japanese Yen
+  AUD: "AU", // Australian Dollar
+  CAD: "CA", // Canadian Dollar
+  CHF: "CH", // Swiss Franc
+  CNY: "CN", // Chinese Yuan
+  HKD: "HK", // Hong Kong Dollar
+  NZD: "NZ", // New Zealand Dollar
+  SGD: "SG", // Singapore Dollar
+  BRL: "BR", // Brazilian Real
+  ZAR: "ZA", // South African Rand
+  KRW: "KR", // South Korean Won
+  MXN: "MX", // Mexican Peso
+  IDR: "ID", // Indonesian Rupiah
+  TRY: "TR", // Turkish Lira
+  THB: "TH", // Thai Baht
+  PHP: "PH", // Philippine Peso
+  MYR: "MY", // Malaysian Ringgit
+  PLN: "PL", // Polish Zloty
+  SEK: "SE", // Swedish Krona
+  NOK: "NO", // Norwegian Krone
+  DKK: "DK", // Danish Krone
+  HUF: "HU", // Hungarian Forint
+  CZK: "CZ", // Czech Koruna
+  ILS: "IL", // Israeli New Shekel
+};
